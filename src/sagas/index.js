68c1f99c0f5f765fbb4ae7230247ac8abed73e0f
iamdarshshah <imdarshshah@gmail.com>
2020-04-07 17:32:23 +0530

    implementation of Channels in saga and added remaining sagas.

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,5 +1,10 @@
 export { currentUserSaga } from "./currentUserSaga";
 export { fetchCartSaga } from "./fetchCartSaga";
 export { itemDetailsSaga } from "./itemDetailsSaga";
-export { itemQuantitySaga } from "./itemQuantitySaga";
 export { itemPriceSaga } from "./itemPriceSaga";
+export { itemQuantitySaga } from "./itemQuantitySaga";
+export { shippingSaga } from "./shippingSaga";
+export { taxRateSaga } from "./taxRateSaga";
+export { checkoutAvailabilitySaga } from "./checkoutAvailabilitySaga";
+export { customerServiceAvailabilitySaga } from "./customerServiceAvailabilitySaga";
+export { checkoutSaga } from "./checkoutSaga";
---
+++ src/sagas/shippingSaga.js
@@ -0,0 +1,43 @@
+import { select, put, takeLatest } from "redux-saga/effects";
+import fetch from "isomorphic-fetch";
+
+import {
+  SET_CART_ITEMS,
+  INCREASE_ITEM_QUANTITY,
+  DECREASE_ITEM_QUANTITY,
+  FETCHED,
+  FETCHING,
+  setShippingFetchStatus,
+  setShippingCost,
+} from "./../actions";
+
+import { cartItemsSelector } from "../selectors";
+
+function* shipping() {
+  yield put(setShippingFetchStatus(FETCHING));
+  const items = yield select(cartItemsSelector);
+
+  // Turn all the item IDs into an API compatible string, and trim the last comma
+  const itemRequestString = items
+    .reduce((string, item) => {
+      for (let i = 0; i < item.get(`quantity`); i++) {
+        string += item.get(`id`) + ",";
+      }
+      return string;
+    }, "")
+    .replace(/,\s*$/, "");
+
+  const response = yield fetch(
+    `http://localhost:8081/shipping/${itemRequestString}`
+  );
+  const { total } = yield response.json();
+  yield put(setShippingCost(total));
+  yield put(setShippingFetchStatus(FETCHED));
+}
+
+export function* shippingSaga() {
+  yield takeLatest(
+    [SET_CART_ITEMS, INCREASE_ITEM_QUANTITY, DECREASE_ITEM_QUANTITY],
+    shipping
+  );
+}
